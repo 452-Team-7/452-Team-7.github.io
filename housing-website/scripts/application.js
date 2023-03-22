7dfb45bad804527ec1fdd62f125699129b8e69a7
Aditya Sharma <adityas0911@gmail.com>
2023-03-22 19:48:12 -0400

    commit

--- housing-website/scripts/application.js
+++ housing-website/scripts/application.js
@@ -1,8 +1,7 @@
-const http = require("http");
-const querystring = require('querystring'); 
-const mysql = require('mysql');
+const http = require("http"); // Web server module, loaded using "require" - waits for HTTP requests from clients.
+const querystring = require('querystring'); // Utilities for parsing and formatting URL query strings.
+const mysql = require('mysql'); // MySQL database driver.
 const port = (process.env.port || 8080); // Port = 8080.
-
 const housingDatabase = mysql.createConnection(
   {
     host: "localhost",
@@ -11,28 +10,46 @@ const housingDatabase = mysql.createConnection(
     dataBase: "Housing_Databse"
   }
 ) // MySQL database
+housingDatabase.connect(function(err)
+  {
+    if(err)
+      throw err;
+    console.log("Housing databse connected."); // Checks for connection.
 
-housingDatabase.connect(function(err) {
- if(err) throw err ; 
- console.log(result) ; 
-
- housingDatabase.query("CREATE TABLE LISTING(Price MONEY, Location VARCHAR(100), Availability BOOL, Transportation VARCHAR(100), Rooms SMALLINT" ,
- function(err, result){
- if(err) throw err ; 
- console.log("New table created") ; }) ; // table created in database
-
- housingDatabase.query("insert into listing(Price , Location, Availability, Transportation, Rooms) values(100.67 , 'Rutgers' , 1 , 'Bus' , 3) " ,
- function(err , result){
- if(err) throw err ; 
- console.log("Inserted into table") ; }) ;
-})
-console.log("connected") ; 
-
-function applicationServer( request , response ) 
+    housingDatabase.query("CREATE TABLE LISTING(price FLOAT, location CHAR(100), availability BOOL, transportation CHAR(100), rooms INT)", function(err, result)
+      {
+        if(err)
+          throw err;
+        console.log("New table created."); // Checks for table creation.
+      }
+    );
+    
+    housingDatabase.query("insert into listing(Price , Location, Availability, Transportation, Rooms) values(100.67 , 'Rutgers' , 1 , 'Bus' , 3)", function(err , result)
+      {
+        if(err)
+          throw err;
+        console.log("Inserted into table."); // Checks for table insertion.
+      }
+    );
+  }
+);
+function applicationServer(request, response)
 {
- response.writeHead(200, {'Content-Type': 'text/plain'}) ; 
- response.write('Hello World') ; 
- response.end() ; 
-} // idk
+  // To be defined later. Finalize the HTTP response message for the client.
+  if (!resMsg.headers || resMsg.headers === null)
+  {
+    resMsg.headers = {};
+  }
+  if (!resMsg.headers["Content-Type"])
+  {
+    resMsg.headers["Content-Type"] = "application/json";
+  }
+  // Send the response message.
+  response.writeHead(resMsg.code, resMsg.hdrs),
+  response.end(resMsg.body);
+  //  response.writeHead(200, {'Content-Type': 'text/plain'});
+  //  response.write('Hello World');
+  //  response.end();
+}
 const webServer = http.createServer(applicationServer);
 webServer.listen(port);
