e47f79839a35d658ffe1283413c141c60b8c79a4
rpatel747 <rushabh.patel747@gmail.com>
2023-03-24 14:26:16 -0400

    Cleaned up backend messages,did some error handling

--- housing-website-v3/server/server.js
+++ housing-website-v3/server/server.js
@@ -1,3 +1,4 @@
+const e = require('express');
 var express = require('express');
 const mysql = require('mysql');
 var app = express();
@@ -19,7 +20,7 @@ connection.connect(
         return console.error("error: " + err.message);
       }
   
-      console.log("Connected to database");
+      console.log("Connected to GCP MySQL database");
       connection.query("CREATE TABLE IF NOT EXISTS listing(provider_username VARCHAR(100),street_address VARCHAR(100),city VARCHAR(100),state VARCHAR(100),zipcode CHAR(5),building_type VARCHAR(100),purchase_type CHAR(4),price FLOAT, availability BOOL, transportation CHAR(100), rooms INT, PRIMARY KEY (street_address,city,state))", function(err, result){
           if(err)
           {
@@ -27,6 +28,7 @@ connection.connect(
           }
           console.log("'listing' table created"); // Checks for table creation.
         });
+
       connection.query("CREATE TABLE IF NOT EXISTS message(chatroomID INT, sender_username VARCHAR(100), message_content VARCHAR(200), datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)",function(err,results){
         if(err) throw err;
         console.log("'message' table created");
@@ -68,13 +70,13 @@ app.post('/signup',function(req,res){
     connection.query(check_user_query,[new_username],function(err,result){
       if(err) throw err;
       if (result.length > 0) {
-        res.status(409).send("Account already exists with given username");
+        res.status(409).send({"message": "Account already exists with given username"});
       }
       else{
         var create_user_account = 'INSERT INTO account(username,password,role) VALUES (?,?,?)';
         connection.query(create_user_account,[new_username,new_password,new_role],function(err,result){
           if(err) throw RegExp;
-          res.status(200).send("Account successfully created"); 
+          res.status(200).send({"message":"Account successfully created"}); 
         });       
       }
 
@@ -92,10 +94,10 @@ app.post('/login',function(req,res){
     if (err) throw err;
   
     if (results.length == 0){
-      res.status(409).send("Login unsucessful, please check your credentials and try again")
+      res.status(409).send({"message": "Login unsucessful, please check your credentials and try again"})
     }
     else{
-        res.status(200).send("Login successful");
+        res.status(200).send({"message":"Login successful"});
     }
   });
 });
@@ -108,7 +110,7 @@ app.get('/listings',function(req,res){
   const search_listing_query = connection.query(listing_query,[search_zipcode],function(err,result){
     if (err) throw err;
     if (result.length == 0) {
-      res.status(200).send("There are no listings for your area");
+      res.status(200).send({"message": "There are no listings for your area"});
     }
     else{
       res.status(200).send(result);
@@ -130,24 +132,42 @@ app.post('/listings',function(req,res){
   const transportation = req.body.transportation;
   const rooms = req.body.rooms;
 
-  // Check if there exists a listing with these details, if it does than another housing_provider owns that listing
-  var check_listing = 'SELECT * FROM listing where street_address=? AND city=? AND state=?';
-  connection.query(check_listing,[street_address,city,state],function(err,result){
+
+  // Check if the user trying to place a listing is a housing_provider, only housing_providers can place reviews
+  var check_user_role = "SELECT * FROM account WHERE username=? AND role=?";
+  connection.query(check_user_role,[provider_username,"housing-provider"],function(err,result){
     if (err) throw err;
-    if (result.length != 0) {
-      res.status(409).send("Another housing provider owns this listing");
+
+    // User is not a housing_provider
+    if (result.length == 0){
+      res.status(400).send({"message":"Listing not posted, only housing providers can post listings"});
       return;
     }
-  });
 
-  // No other listings exist matching the given details, create new listing
-  var place_listing = 'INSERT INTO listing(provider_username,street_address,city,state,zipcode,building_type,purchase_type,price,availability,transportation,rooms) values(?,?,?,?,?,?,?,?,?,?,?)';
-  connection.query(place_listing, [provider_username,street_address,city,state,zipcode,building_type,purchase_type,price,availability,transportation,rooms], function(err,result){
-    if (err) throw err;
-    res.status(200).send(req.body);
+    // User is a housing provider
+    else{
+      // Check if there exists a listing with these details, if it does than another housing_provider owns that listing
+      var check_listing = 'SELECT * FROM listing where street_address=? AND city=? AND state=?';
+      connection.query(check_listing,[street_address,city,state],function(err,result){
+        if (err) throw err;
+
+        // Another housing provider owns this listing, return status 409
+        if (result.length != 0) {
+          res.status(409).send({"message": "Listing already exists, another housing provider owns this listing"});
+          return;
+        }
+
+        // No other listings exist matching the given details, create new listing
+        var place_listing = 'INSERT INTO listing(provider_username,street_address,city,state,zipcode,building_type,purchase_type,price,availability,transportation,rooms) values(?,?,?,?,?,?,?,?,?,?,?)';
+        connection.query(place_listing, [provider_username,street_address,city,state,zipcode,building_type,purchase_type,price,availability,transportation,rooms], function(err,result){
+          if (err) throw err;
+          res.status(200).send(req.body);
+          });
+      });
     }
-  );
+  });
 });
+
 
 
 app.post('/listings/post-review',function(req,res){
@@ -161,61 +181,83 @@ app.post('/listings/post-review',function(req,res){
   var listing_exists = "SELECT * FROM listing WHERE street_address=? AND city=? AND state=?";
   connection.query(listing_exists,[street_address,city,state],function(err,result){
     if (err) throw err;
+
+    // Listing does not exist, return status 400
     if (result.length == 0){
-      res.status(400).send("Listing does not exist");
+      res.status(400).send({"message": "Listing does not exist"});
       return;
     }
-  });
 
+    // Check if user trying to place review is a tenant, only tenant's are allowed to place reviews
+    var check_user_role = "SELECT * FROM account WHERE username=? AND role=?";
+    connection.query(check_user_role,[tenant_username,"tenant"],function(err,result){
+      if (err) throw err;
 
-  // Checked that the listing does exist, check if user already has left a review. If they have not then place the review
-  var check_user_review = "SELECT * FROM reviews WHERE street_address=? AND city=? AND state=? AND username=?";
-  connection.query(check_user_review,[street_address,city,state,tenant_username],function(err,result){
-    if (err) throw err;
-    if (result.length != 0){
-      res.status(409).send("A review has a already been placed by the user");
-      return;
-    }
-  });
-  
-  
-  var place_review =  "INSERT INTO reviews(username,street_address,city,state,review) values(?,?,?,?,?); SELECT * FROM reviews WHERE street_address=? AND city=? AND state=?";
-  connection.query(place_review,[tenant_username,street_address,city,state,review,street_address,city,state,tenant_username],function(err,result){
-    if (err) throw err;
-    if (result.length!=0){
-      res.status(200).send(result[1]);
-    }
+      // User is a tenant 
+      if(result.length!=0){
+        // Check if user already has left a review. If they have not then place the review
+        var check_user_review = "SELECT * FROM reviews WHERE street_address=? AND city=? AND state=? AND username=?";
+        connection.query(check_user_review,[street_address,city,state,tenant_username],function(err,result){
+          if (err) throw err;
+    
+          // User already left a review for this building, return status 409
+          if (result.length != 0){
+            res.status(409).send({"message": "A review has a already been placed by the user"});
+            return;
+          }
+    
+          var place_review =  "INSERT INTO reviews(username,street_address,city,state,review) values(?,?,?,?,?); SELECT * FROM reviews WHERE street_address=? AND city=? AND state=?";
+          connection.query(place_review,[tenant_username,street_address,city,state,review,street_address,city,state,tenant_username],function(err,result){
+            if (err) throw err;
+            if (result.length!=0){
+              res.status(200).send(result[1]);
+            }
+          });
+        });       
+      }
+
+      // User is not a tenant
+      else{
+        res.status(400).send({"message":"Review could not be placed, only tenant's can place reviews"});
+      }
+
+    });
   });
 });
+
+
 
 
 
 app.get('/listings/get-reviews', function(req,res){
-
     const street_address = req.body.street_address;
     const city = req.body.city;
     const state = req.body.state;
+
+
   // Check if the listing actually exists
     var listing_exists = "SELECT * FROM listing WHERE street_address=? AND city=? AND state=?";
     connection.query(listing_exists,[street_address,city,state],function(err,result){
       if (err) throw err;
+
+      // Listing does not exist, return status 400
       if (result.length == 0){
-        res.status(400).send("Listing does not exist");
+        res.status(400).send({"message": "Listing does not exist"});
         return;
       }
+
+      // Return all the reviews for the listing
+      var get_reviews = "SELECT * FROM reviews WHERE street_address=? AND city=? AND state=?";
+      connection.query(get_reviews,[street_address,city,state],function(err,result){
+        if (err) throw err;
+        if (result.length!=0){
+          res.status(200).send(result);
+        }
+      });
     });
-
-
-  // Return all the reviews for the listing
-    var get_reviews = "SELECT * FROM reviews WHERE street_address=? AND city=? AND state=?";
-    connection.query(get_reviews,[street_address,city,state],function(err,result){
-      if (err) throw err;
-      if (result.length!=0){
-        res.status(200).send(result);
-      }
-    });
-
 });
+
+
 
 app.post('/message/create-chat',function(req,res){
   const participant_one_username = req.body.username_one;
@@ -227,7 +269,7 @@ app.post('/message/create-chat',function(req,res){
   connection.query(check_users_exist,[participant_one_username,participant_two_username],function(err,result){
     if(err) throw err;
     if (result.length != 2 || (result[0].length == 0  || result[1].length == 0) ){
-      res.status(400).send("One or more users does not exist.");
+      res.status(400).send({"message": "One or more users does not exist."});
       return;
     }
     else{
@@ -237,7 +279,7 @@ app.post('/message/create-chat',function(req,res){
       connection.query(check_chatroom_exists,[participant_one_username,participant_two_username,participant_two_username,participant_one_username],function(err,result){
         if (err) throw err;
         if (result.length != 0){
-            res.status(409).send("Chatroom could not be created because a chatroom exists between both users");
+            res.status(409).send({"message": "Chatroom could not be created because a chatroom exists between both users"});
             return;
         }
         else{
@@ -246,13 +288,43 @@ app.post('/message/create-chat',function(req,res){
           var create_chatroom = "INSERT INTO chatroom(participant_one_username,participant_two_username) values(?,?)";
           connection.query(create_chatroom,[participant_one_username,participant_two_username],function(err,result){
             if (err) throw err;
-            res.status(200).send("Chatroom successfully created");
+            res.status(200).send({"message": "Chatroom successfully created"});
           });
         }
       });
     }
   });
 });
+
+
+
+app.post('/message/send',function(req,res){
+  const sender_username = req.body.sender_username;
+  const receiver_username = req.body.receiver_username;
+  const message_content = req.body.message_content;
+
+  var check_users_exist = "SELECT * FROM account WHERE username=?;SELECT * FROM account WHERE username=?";
+  connection.query(check_users_exist,[sender_username,receiver_username],function(err,result){
+    if (err) throw err;
+    if (result.length != 2 || (result[0].length == 0  || result[1].length == 0) ){
+      res.status(400).send({"message": "One or more users does not exist."});
+      return;
+    }
+    var check_chatroom_exists = "SELECT chatroomID FROM chatroom WHERE (participant_one_username=? AND participant_two_username=?) OR (participant_one_username=? AND participant_two_username=?)";
+    connection.query(check_chatroom_exists,[sender_username,receiver_username,receiver_username,sender_username],function(err,result){
+      if(err) throw err;
+      // A chatroom exists between the two users, post the given message
+      if (result.length != 0){
+        var post_message = "INSERT INTO message(chatroomID,sender_username,message_content) values(?,?,?)";
+        connection.query(post_message,[result[0].chatroomID,sender_username,message_content],function(err,result){
+          if (err) throw err;
+          res.status(200).send({"message": "Message sent successfully"});
+        })
+      }
+    });
+  });  
+});
+
 
 
 app.get('/message/open-chat',function(req,res){
@@ -263,7 +335,7 @@ app.get('/message/open-chat',function(req,res){
   connection.query(check_users_exist,[participant_one_username,participant_two_username],function(err,result){
     if (err) throw err;
     if (result.length != 2 || (result[0].length == 0  || result[1].length == 0) ){
-      res.status(400).send("One or more users does not exist.");
+      res.status(400).send({"message": "One or more users does not exist."});
       return;
     }
     
@@ -282,7 +354,7 @@ app.get('/message/open-chat',function(req,res){
         }
         // Chatroom does not exist between users, send status 400.
         else{
-          res.status(400).send("No chat exists between users");
+          res.status(400).send({"message": "No chat exists between users"});
         }
       });
 
@@ -291,38 +363,12 @@ app.get('/message/open-chat',function(req,res){
 });
 
 
-app.post('/message/send',function(req,res){
-  const sender_username = req.body.sender_username;
-  const receiver_username = req.body.receiver_username;
-  const message_content = req.body.message_content;
-
-  var check_users_exist = "SELECT * FROM account WHERE username=?;SELECT * FROM account WHERE username=?";
-  connection.query(check_users_exist,[sender_username,receiver_username],function(err,result){
-    if (err) throw err;
-    if (result.length != 2 || (result[0].length == 0  || result[1].length == 0) ){
-      res.status(400).send("One or more users does not exist.");
-      return;
-    }
-    var check_chatroom_exists = "SELECT chatroomID FROM chatroom WHERE (participant_one_username=? AND participant_two_username=?) OR (participant_one_username=? AND participant_two_username=?)";
-    connection.query(check_chatroom_exists,[sender_username,receiver_username,receiver_username,sender_username],function(err,result){
-      if(err) throw err;
-      // A chatroom exists between the two users, post the given message
-      if (result.length != 0){
-        var post_message = "INSERT INTO message(chatroomID,sender_username,message_content) values(?,?,?)";
-        connection.query(post_message,[result[0].chatroomID,sender_username,message_content],function(err,result){
-          if (err) throw err;
-          res.status(200).send("Message sent");
-        })
-      }
-    });
-  });  
-});
-
-
 
 // Start the application server 
 var server = app.listen(PORT,() =>{
-    console.log('server is running');
+    console.log('Server has started, listenting at PORT: ' + PORT);
 });
 
 module.exports = server;
+
+
